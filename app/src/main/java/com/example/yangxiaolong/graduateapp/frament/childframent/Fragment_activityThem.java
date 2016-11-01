1655550293546a88fcdfd3cdba61a3e69c8204fd
hellowenqiang <997378837@qq.com>
2016-11-01 13:30:24 +0800

    我写了7个Fragment

---
+++ app/src/main/java/com/example/yangxiaolong/graduateapp/frament/childframent/Fragment_activityThem.java
@@ -0,0 +1,30 @@
+package com.example.yangxiaolong.graduateapp.frament.childframent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yangxiaolong.graduateapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_activityThem extends Fragment {
+
+
+    public Fragment_activityThem() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity_them, container, false);
+    }
+
+}
