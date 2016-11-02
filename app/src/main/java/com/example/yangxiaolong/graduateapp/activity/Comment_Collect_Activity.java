46d098564585b321b6c06a1e5502ba74930b7280
hellowenqiang <997378837@qq.com>
2016-11-02 20:40:43 +0800

    我创建了消息界面

---
+++ app/src/main/java/com/example/yangxiaolong/graduateapp/activity/Comment_Collect_Activity.java
@@ -0,0 +1,15 @@
+package com.example.yangxiaolong.graduateapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yangxiaolong.graduateapp.R;
+
+public class Comment_Collect_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment__collect);
+    }
+}
