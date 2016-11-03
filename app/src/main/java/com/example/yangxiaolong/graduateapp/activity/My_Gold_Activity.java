54f1038323ac00bc2eed0d1c9b91ea0b3e8df8a5
callmenero <1125548362@qq.com>
2016-11-03 15:23:54 +0800

    完善登录界面

---
+++ app/src/main/java/com/example/yangxiaolong/graduateapp/activity/My_Gold_Activity.java
@@ -0,0 +1,16 @@
+package com.example.yangxiaolong.graduateapp.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yangxiaolong.graduateapp.R;
+
+public class My_Gold_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__gold_);
+    }
+}
